6fdbeee506f86fd7bb0f46030ec70aa58a3eb7f9
Marcin Karpiński <118042924+Cobramarcin@users.noreply.github.com>
2023-06-30 17:22:57 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/test", (_req, res) => {
-    res.status(200).send("Hello World")
+    res.status(200).send("Programowanie Zespołowe")
 });
 
 module.exports = app;
